36cd21788add07e194e2efb6463ce853c19f91f6
rb58853 <rb58853@gmail.com>
2024-04-04 11:20:35 -0400

    simple post and get

--- src/Controllers/UsersContoller.cs
+++ src/Controllers/UsersContoller.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
 using Backend.Models;
 using Newtonsoft.Json;
+using Backend.Context;
+using Microsoft.EntityFrameworkCore;
 
 namespace Backend.Controllers
 {
@@ -8,19 +10,42 @@ namespace Backend.Controllers
     [Route("[controller]")]
     public class UsersController : ControllerBase
     {
-        User user = new User
+        private readonly AppDbContext context;
+
+        public UsersController(AppDbContext context)
         {
-            Id = "raul",
-            Name = "Raul",
-            LastName = "Beltran Gomez",
-            Email = "rb58853@gmal.com",
-            Images = ["path1", "path2"]
-        };
+            this.context = context;
+        }
+
+        // GET: api/Users
         [HttpGet]
-        public string Get()
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
         {
-            string jsonUser = JsonConvert.SerializeObject(user);
-            return jsonUser;
+            return await context.users.ToListAsync();
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            context.users.Add(user);
+            await context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUser", new { id = user.Id }, user);
+        }
+
+        // GET: api/Users/"id"
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(string id)
+        {
+            var user = await context.users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
         }
     }
 }
